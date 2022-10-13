1ce60e4ba92bf7bc18f0e037c923ade295fb040a
Yeet <rasmusmkn@gmail.com>
2022-10-13 18:18:39 +0200

    Probably final commit - Small fixes and analysis

--- Program.cs
+++ Program.cs
@@ -3,58 +3,44 @@
     internal class Program
     {
 
-        static ulong diff = 0x0080820060000000;
+        static ulong diff = 0x405c000004000000;
         static ulong diffOut = 0x6000000000000000;
 
-        public static void Main(string[] args)
+        static ulong alpha = 0x2104008000008000;
+        static ulong delta = 0x0104008000011000;
+
+        static int n = 10000;
+        static float runs = 10;
+
+        public static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+
+        public static async Task MainAsync(string[] args)
         {
-
-            int prob = 0;
-            for (int i = 0; i < 10000; i++)
+            int totalProb = 0;
+            BitArray key = GenerateRandom(64);
+            List<Task<int>> tasks = new List<Task<int>>();
+            for (int i = 0; i < runs; i++)
             {
-                BitArray key = GenerateRandom(64);
-                BitArray message = GenerateRandom(64);
-                BitArray messageCopy = message.DeepCopy();
-                messageCopy = messageCopy.Xor(DES.FromValue(diff,64));
-
-                key = key.Reverse();
-                message = message.Reverse();
-                messageCopy = messageCopy.Reverse();
-
-                DESKeyScheduler scheduler = new DESKeyScheduler(key, 2);
-
-                scheduler.GenerateKeys();
-
-                DES des = new(scheduler);
-
-                BitArray ciph1 = des.Crypt(key,message,2, false);
-                BitArray ciph2 = des.Crypt(key, messageCopy,2, false);
-
-                BitArray res = ciph1.Xor(ciph2);
-                
-                if(Same(DES.FromValue(diffOut, 64).Reverse(), res))
-                {
-                    prob++;
-                }
-
-
-
-
-
-                //Console.WriteLine("Input:");
-
-                //Console.WriteLine("Message: " + DESKeyScheduler.ToPrint(message.Reverse().AsLong(), 8, 64));
-
-                //Console.WriteLine("Key: " + DESKeyScheduler.ToPrint(key.Reverse().AsLong(), 8, 64));
-
-                //Console.WriteLine("\n--------------------------------------------------------------------\n");
-
-
-
-                
+                Task<int> run = Task.Run(() => { return DoRunLin(n,key); });
+                tasks.Add(run);
             }
 
-            Console.WriteLine(prob/10000.0);
+            foreach(var v in tasks)
+            {
+                int val = await v;
+                if(val < n/2f)
+                {
+                    totalProb += val;
+                }
+                else
+                {
+                    totalProb += n-val;
+                }
+                Console.WriteLine(val);
+            }
+
+            Console.WriteLine(Math.Abs((totalProb / (n * runs))));
+            Console.WriteLine(Math.Abs((totalProb / (n * runs)) - 0.5f));
 
 
 
@@ -124,7 +110,7 @@
 
             //Console.WriteLine(final.Reverse().Append(splitMess[1].Reverse()).AsLong().ToString("x"));
 
-            
+
 
 
 
@@ -176,6 +162,90 @@
             }
 
             return true;
+        }
+
+        public static int DoRun(int n)
+        {
+            int prob = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                BitArray key = GenerateRandom(64);
+                BitArray message = GenerateRandom(64);
+                BitArray messageCopy = message.DeepCopy();
+                messageCopy = messageCopy.Xor(DES.FromValue(diff, 64));
+
+                key = key.Reverse();
+                message = message.Reverse();
+                messageCopy = messageCopy.Reverse();
+
+                DESKeyScheduler scheduler = new DESKeyScheduler(key, 6);
+
+                scheduler.GenerateKeys();
+
+                DES des = new(scheduler);
+
+                BitArray ciph1 = des.Crypt(key, message, 5, false);
+                BitArray ciph2 = des.Crypt(key, messageCopy, 5, false);
+
+                BitArray res = ciph1.Xor(ciph2);
+
+                if (Same(DES.FromValue(diff, 64).Reverse(), res))
+                {
+                    prob++;
+                }
+            }
+
+            return prob;
+        }
+
+        public static int DoRunLin(int n,BitArray key )
+        {
+            int count = 0;
+            BitArray alphaArr = DES.FromValue(alpha, 64);
+            alphaArr = alphaArr.Reverse();
+
+            BitArray deltaArr = DES.FromValue(delta, 64);
+            deltaArr = deltaArr.Reverse();
+
+            
+            DESKeyScheduler scheduler = new DESKeyScheduler(key, 7);
+
+            scheduler.GenerateKeys();
+            for (int i = 0; i < n; i++)
+            {
+                BitArray message = GenerateRandom(64);
+
+                int scalarMA = Scalar(message, alphaArr);
+
+                DES des = new(scheduler);
+
+                BitArray ciph1 = des.Crypt(key, message, 3, false);
+
+                int scalarMB = Scalar(ciph1, alphaArr);
+
+                if((scalarMA ^ scalarMB) == 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static int Scalar(BitArray arr1, BitArray arr2)
+        {
+            int res = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if(arr1[i] && arr2[i])
+                {
+                    res = (res + 1) % 2;
+                }
+            }
+
+            return res;
         }
     }
 }
